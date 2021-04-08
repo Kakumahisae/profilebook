54449e5910928b0c1ed7e9931d08b1dcaa9843a0
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-04-08 02:34:26 +0000

    4/8朝

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'homes/help'
+  
+  resources :stories
+  
 end
