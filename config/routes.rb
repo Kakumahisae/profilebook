70d6db648d3c1adba63aed60db3d561919162e02
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-04-10 13:50:39 +0000

    4/10night

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'homes#top'
   get 'homes/help'
+  
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+  
   
   resources :stories
   resources :users
