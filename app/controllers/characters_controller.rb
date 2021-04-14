0f2f17e38bda6bf624d0beeef6230fee4f0d33c6
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-04-14 06:48:23 +0000

    4/14

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,14 +6,23 @@ class CharactersController < ApplicationController
   end
   
   def new
+    @story = Story.find_by(id: params[:story_id])
+    @character = Character.new
   end
   
   def create
+    @story = Story.find_by(id: params[:story_id])
+    render :index
   end
   
   def show
   end
   
   def destroy
+  end  
+  
+  private
+  def character_params
+    params.require(:character).permit(:name,:gender)
   end  
 end
