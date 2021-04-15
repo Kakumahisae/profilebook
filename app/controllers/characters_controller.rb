7fb371f8e405ee4ae8f1929110f5e9dea7ea8b44
KakumaHisae <mabel8mapel@yahoo.co.jp>
2021-04-15 01:31:32 +0000

    4/15

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,16 +1,20 @@
 class CharactersController < ApplicationController
   
+  #なぜ動いたのか
   def index
     @user = current_user
     @story = Story.find_by(id: params[:story_id])
+    @characters = @story.characters
   end
   
+  #character.story.title
   def new
     @story = Story.find_by(id: params[:story_id])
     @character = Character.new
   end
   
   def create
+    @story = Story.find_by(id: params[:story_id])
     @character = Character.new(character_params)
     if @character.save
       redirect_to story_characters_path, success: '新しく人物を追加しました'
@@ -25,6 +29,7 @@ class CharactersController < ApplicationController
   def destroy
   end  
   
+  #story_idがなかった
   private
   def character_params
     params.require(:character).permit(:name,:gender,:story_id)
